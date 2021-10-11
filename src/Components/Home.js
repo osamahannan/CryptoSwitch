b1e55bf8990a35468872c91098b6d002340fd80d
osamahannan <83069516+osamahannan@users.noreply.github.com>
2021-10-11 13:45:28 +0530

    displayed data of all cryptocurrencies

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -4,11 +4,13 @@ import axios from "axios";
 const Coindata = () => {
 
     const [coins, setCoins] = useState([]);
+    // const [name, setName] = useState("");
 
     useEffect(() => {
         axios.get("https://api.coingecko.com/api/v3/coins/markets?vs_currency=INR&order=market_cap_desc&per_page=10&page=1&sparkline=false")
             .then(res => {
                 setCoins(res.data);
+                console.log(coins[0].id);
             }).catch(error => {
                 console.log(error);
             })
@@ -22,111 +24,33 @@ const Coindata = () => {
 
                 <div className="coin-container">
 
-                    <div className="coin-detail">
+                    {coins.map(coin => {
+                        return (
+                            <div className="coin-detail">
 
-                        <div className="coin-info">
-                            <img src="https://assets.coingecko.com/coins/images/1/large/bitcoin.png?1547033579" className="coin-pic" alt="coin pic" />
+                                <div className="coin-info">
+                                    <img src={coin.image} className="coin-pic" alt="coin pic" />
 
-                            <div className="coin-data">
-                                <span className="coin-name">Bitcoin</span>
-                                <span>BTC</span>
+                                    <div className="coin-data">
+                                        <span className="coin-name">{coin.name}</span>
+                                        <span>{coin.symbol}</span>
+                                    </div>
+                                </div>
+
+                                <div className="coin-current">
+                                    <span>{coin.current_price}</span>
+                                    <span>{coin.price_change_percentage_24h}</span>
+                                </div>
+
+                                <div className="wallet-button">
+                                    <button className="btn">Add to Wallet</button>
+                                </div>
+
                             </div>
-                        </div>
+                        )
+                    })}
 
-                        <div className="coin-current">
-                            <span>Rs 42,89,918.15</span>
-                            <span>+1.64%</span>
-                        </div>
 
-                        <div className="wallet-button">
-                            <button className="btn">Add to Wallet</button>
-                        </div>
-
-                    </div>
-                    <div className="coin-detail">
-
-                        <div className="coin-info">
-                            <img src="https://assets.coingecko.com/coins/images/1/large/bitcoin.png?1547033579" className="coin-pic" alt="coin pic" />
-
-                            <div className="coin-data">
-                                <span className="coin-name">Bitcoin</span>
-                                <span>BTC</span>
-                            </div>
-                        </div>
-
-                        <div className="coin-current">
-                            <span>Rs 42,89,918.15</span>
-                            <span>+1.64%</span>
-                        </div>
-
-                        <div className="wallet-button">
-                            <button className="btn">Add to Wallet</button>
-                        </div>
-
-                    </div>
-                    <div className="coin-detail">
-
-                        <div className="coin-info">
-                            <img src="https://assets.coingecko.com/coins/images/1/large/bitcoin.png?1547033579" className="coin-pic" alt="coin pic" />
-
-                            <div className="coin-data">
-                                <span className="coin-name">Bitcoin</span>
-                                <span>BTC</span>
-                            </div>
-                        </div>
-
-                        <div className="coin-current">
-                            <span>Rs 42,89,918.15</span>
-                            <span>+1.64%</span>
-                        </div>
-
-                        <div className="wallet-button">
-                            <button className="btn">Add to Wallet</button>
-                        </div>
-
-                    </div>
-                    <div className="coin-detail">
-
-                        <div className="coin-info">
-                            <img src="https://assets.coingecko.com/coins/images/1/large/bitcoin.png?1547033579" className="coin-pic" alt="coin pic" />
-
-                            <div className="coin-data">
-                                <span className="coin-name">Bitcoin</span>
-                                <span>BTC</span>
-                            </div>
-                        </div>
-
-                        <div className="coin-current">
-                            <span>Rs 42,89,918.15</span>
-                            <span>+1.64%</span>
-                        </div>
-
-                        <div className="wallet-button">
-                            <button className="btn">Add to Wallet</button>
-                        </div>
-
-                    </div>
-                    <div className="coin-detail">
-
-                        <div className="coin-info">
-                            <img src="https://assets.coingecko.com/coins/images/1/large/bitcoin.png?1547033579" className="coin-pic" alt="coin pic" />
-
-                            <div className="coin-data">
-                                <span className="coin-name">Bitcoin</span>
-                                <span>BTC</span>
-                            </div>
-                        </div>
-
-                        <div className="coin-current">
-                            <span>Rs 42,89,918.15</span>
-                            <span>+1.64%</span>
-                        </div>
-
-                        <div className="wallet-button">
-                            <button className="btn">Add to Wallet</button>
-                        </div>
-
-                    </div>
                 </div>
             </div>
         </>
