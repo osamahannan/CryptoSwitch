a1aabc632f7795e7f0929f6b928c1e7113fb9d69
osamahannan <83069516+osamahannan@users.noreply.github.com>
2021-10-14 13:46:08 +0530

    displayed data on graph

--- src/Components/LineChart.js
+++ src/Components/LineChart.js
@@ -1,16 +1,31 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import { Line } from 'react-chartjs-2';
+import axios from "axios";
 
 const LineChart = () => {
+
+    const [history, setHistory] = useState([]);
+
+    useEffect(() => {
+        axios.get("https://api.coingecko.com/api/v3/coins/bitcoin/market_chart?vs_currency=INR&days=15&interval=daily")
+            .then(res => {
+                setHistory(res.data.prices.filter(coindata => 
+                    coindata[1]
+                ))
+            }).catch(error => {
+                console.log(error);
+            })
+    }, [])
+
     return (
         <div className="chart">
             <Line
                 data={{
-                    labels: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15'],
+                    labels: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16'],
                     datasets: [
                         {
                             label: '# of votes',
-                            data: [12, 19, 3, 4, 2, 3, 6, 14, 10, 17, 18, 1, 5, 16, 15],
+                            data: [history[0], history[1], history[2], history[3], history[4], history[5], history[6], history[7], history[8], history[9], history[10], history[11], history[12], history[13], history[14], history[15]],
                             borderColor: 'rgb(75, 192, 192)'
                         }
                     ]
