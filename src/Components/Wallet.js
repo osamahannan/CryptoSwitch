d386153a7c48d532b395f5f73c4e2d4211e2d98b
osamahannan <83069516+osamahannan@users.noreply.github.com>
2021-10-25 11:59:35 +0530

    updated volume of coin

--- src/Components/Wallet.js
+++ src/Components/Wallet.js
@@ -2,50 +2,50 @@ import React from 'react'
 
 const Wallet = ({ filteredWallet, parent, setParent }) => {
 
-    const increaseHandle = (wallets) => {
-        const localvolume = wallets.coinvolume
-        setParent(parent.map((el) => (el.id === wallets.id) ? {...el, coinvolume: localvolume+1} : el))
+    const increaseHandle = (wallet) => {
+        // const localvolume = wallet.coinvolume
+        setParent(parent.map((el) => (el.id === wallet.id && wallet.coinvolume > 0) ? {...el, coinvolume: wallet.coinvolume+1} : el))
     }
 
-    const decreaseHandle = (wallets) => {
-        const localvolume = wallets.coinvolume
-        setParent(parent.map((el) => (el.id === wallets.id) ? {...el, coinvolume: localvolume-1} : el))
+    const decreaseHandle = (wallet) => {
+        // const localvolume = wallet.coinvolume
+        setParent(parent.map((el) => (el.id === wallet.id && wallet.coinvolume > 1) ? {...el, coinvolume: wallet.coinvolume-1} : el))
     }
 
-    const deleteHandle = (wallets) => {
-        setParent(parent.filter((el) => el.id !== wallets.id))
+    const deleteHandle = (wallet) => {
+        setParent(parent.filter((el) => el.id !== wallet.id))
     }
 
     return (
         <div className="main-container">
             <div className="wallet-container">
                 <div className="wallet">
-                    {filteredWallet.map(wallets => {
+                    {filteredWallet.map(wallet => {
                         return (
-                            <div className="wallet-coin-detail" key={wallets.id}>
+                            <div className="wallet-coin-detail" key={wallet.id}>
 
                                 <div className="wallet-info">
-                                    <img src={wallets.image} className="coin-pic" alt="coin pic" />
+                                    <img src={wallet.image} className="coin-pic" alt="coin pic" />
 
                                     <div className="coin-data">
-                                        <span className="coin-name">{wallets.name}</span>
-                                        <span>{wallets.symbol}</span>
+                                        <span className="coin-name">{wallet.name}</span>
+                                        <span>{wallet.symbol}</span>
                                     </div>
                                 </div>
 
                                 <div className="coin-current">
-                                    <span> &#x20B9; {wallets.current_price.toLocaleString()}</span>
-                                    <span className={wallets.price_change_percentage_24h > 0 ? "green" : "red"}> {wallets.price_change_percentage_24h > 0 ? `+${wallets.price_change_percentage_24h.toFixed(2)}` : wallets.price_change_percentage_24h.toFixed(2)}%</span>
+                                    <span> &#x20B9; {wallet.current_price.toLocaleString()}</span>
+                                    <span className={wallet.price_change_percentage_24h > 0 ? "green" : "red"}> {wallet.price_change_percentage_24h > 0 ? `+${wallet.price_change_percentage_24h.toFixed(2)}` : wallet.price_change_percentage_24h.toFixed(2)}%</span>
                                 </div>
 
                                 <div className="wallet-edit">
                                     <div className="dec-inc">
-                                        <i className="fas fa-plus" onClick = {()=>increaseHandle(wallets)}></i>
-                                        <span>{wallets.coinvolume}</span>
-                                        <i className="fas fa-minus" onClick = {()=>decreaseHandle(wallets)}></i>
+                                        <i className="fas fa-plus" onClick = {()=>increaseHandle(wallet)}></i>
+                                        <span>{wallet.coinvolume}</span>
+                                        <i className="fas fa-minus" onClick = {()=>decreaseHandle(wallet)}></i>
                                     </div>
 
-                                    <div className="delete" onClick = {() => deleteHandle(wallets)}>
+                                    <div className="delete" onClick = {() => deleteHandle(wallet)}>
                                         <i className="fas fa-trash"></i>
                                     </div>
                                 </div>
