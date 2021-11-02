d0fce77006083889b0f2c181b21e19bbfb2f5001
osamahannan <83069516+osamahannan@users.noreply.github.com>
2021-11-02 13:11:17 +0530

    updated home page

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,15 +1,17 @@
 import React from 'react';
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
+import logopic from '../Assets/CryptoSwitch.png';
 
 const Navbar = () => {
     return (
         <div className="nav-bar">
             <div className="logo">
+                <img src={logopic} className="logo-pic" alt="logo pic" />
                 <h2><span className="crypto">Crypto</span><span className="switch">Switch</span></h2>
             </div>
             <ul>
-                <li><Link to="/" className ="noSelect">Home</Link></li>
-                <li><Link to="/wallet" className ="noSelect">Wallet</Link></li>
+                <li><Link to="/" className="noSelect">Home</Link></li>
+                <li><Link to="/wallet" className="noSelect">Wallet</Link></li>
             </ul>
         </div>
     )
